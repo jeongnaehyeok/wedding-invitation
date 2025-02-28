42acc871d15a52732d918c3b3eeffef564e5dc76
정내혁 <cnh0301@gmail.com>
2025-02-28 14:44:25 +0900

    feat: 글자색 지정 및 사이즈 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,37 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "var(--color-main)",
+        "dark-1": "var(--color-dark-1)",
+        "dark-2": "var(--color-dark-2)",
+        "dark-3": "var(--color-dark-3)",
+        "light-1": "var(--color-light-1)",
+        "light-2": "var(--color-light-2)",
+        "light-3": "var(--color-light-3)",
+        white: "var(--color-white)",
+        black: "var(--color-black)",
+        red: "var(--color-red)",
+        yellow: "var(--color-yellow)",
+        green: "var(--color-green)",
+        base: "var(--color-text-base)",
+      },
+      fontSize: {
+        "2xs": [
+          "0.625rem",
+          {
+            lineHeight: "0.75rem",
+          },
+        ],
+        "3xs": [
+          "0.5rem",
+          {
+            lineHeight: "0.625rem",
+          },
+        ],
+      },
+    },
   },
   plugins: [],
 };
