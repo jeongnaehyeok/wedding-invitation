146908f7afd04bb5348d9159c73f3f1df011aa0d
정내혁 <cnh0301@gmail.com>
2025-02-28 13:21:45 +0900

    feat: 기본 정보 추가

---
+++ src/constants/index.ts
@@ -0,0 +1,18 @@
+// NOTICE: 데이터 형식을 지켜주세요.
+
+export const GROOM = {
+  name: "홍길동",
+  account: {
+    number: "123-456-7890",
+    bank: "국민은행",
+  },
+  phone: "010-1234-5678",
+};
+export const BRIDE = {
+  name: "홍길순",
+  account: {
+    number: "123-456-7890",
+    bank: "국민은행",
+  },
+  phone: "010-1234-5678",
+};
