bf71d1880c4d9a8b5fb6b17434b5b357dd67a7bc
정내혁 <cnh0301@gmail.com>
2025-02-28 16:57:54 +0900

    feat: Calendar component 추가

---
+++ src/components/Calendar/index.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { WEDDING_DATE } from "@/constants";
+
+const Calendar = () => {
+  const [year, month, day] = WEDDING_DATE.split("-").map(Number);
+  const firstDay = new Date(year, month - 1, 1);
+  const lastDay = new Date(year, month, 0);
+
+  const daysInMonth = lastDay.getDate();
+  const startDay = firstDay.getDay();
+
+  const daysArray = Array.from({ length: daysInMonth }, (_, i) => i + 1);
+  const paddingDays = Array.from({ length: startDay }, () => null);
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-5">
+      <h1 className="text-xl text-main">{month.toLocaleString()}월</h1>
+      <div className="grid grid-cols-7 gap-3 text-center">
+        {["일", "월", "화", "수", "목", "금", "토"].map((day, index) => (
+          <div
+            key={day}
+            className={`font-semibold ${index === 0 ? "text-red" : ""}`}
+          >
+            {day}
+          </div>
+        ))}
+        {paddingDays.map((_, i) => (
+          <div key={`empty-${i}`} />
+        ))}
+        {daysArray.map((d) => {
+          const currentDayOfWeek = new Date(year, month - 1, d).getDay();
+          const isTargetDate = d === day;
+          const className = `size-[1.625rem] text-base text-center ${
+            isTargetDate
+              ? "text-white bg-main rounded-full"
+              : currentDayOfWeek === 0
+              ? "text-red"
+              : ""
+          }
+          `;
+
+          return (
+            <div key={d} className={className}>
+              {d}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Calendar;
