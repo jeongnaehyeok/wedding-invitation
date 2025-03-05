26e1a4212329d5d8fb73cb62970e512970dfb5bc
정내혁 <cnh0301@gmail.com>
2025-03-05 23:50:30 +0900

    feat: 위치 정보 아이콘 추가 (Kakao, Naver, TMap)

---
+++ src/components/Location/icons/NaverIcon.tsx
@@ -0,0 +1,15 @@
+const NaverIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="0.75rem"
+    height="0.75rem"
+    viewBox="0 0 24 24"
+  >
+    <path
+      fill="currentColor"
+      d="M16.273 12.845 7.376 0H0v24h7.726V11.156L16.624 24H24V0h-7.727v12.845Z"
+    />
+  </svg>
+);
+
+export default NaverIcon;
